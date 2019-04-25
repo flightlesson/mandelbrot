ba42516cf3cc9139f7364ca4b548379eb3692cfc
Tim Singletary <tsingle@localhost>
2019-04-25 07:45:07 -0400

    added parse(String s)

--- src/main/java/com/strongblackcoffee/mandelbrot/BigComplex.java
+++ src/main/java/com/strongblackcoffee/mandelbrot/BigComplex.java
@@ -1,11 +1,29 @@
 package com.strongblackcoffee.mandelbrot;
 
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
  */
 public class BigComplex {
+    static final String SIMPLE_NAME = MethodHandles.lookup().lookupClass().getSimpleName();
+    static final Logger LOGGER = LogManager.getLogger(SIMPLE_NAME);
+    
+    public static BigComplex parse(String s) {
+        if (s == null) return null;
+        try {
+            String[] a = s.split(",");
+            BigDecimal real = new BigDecimal(a[0]);
+            BigDecimal imag = new BigDecimal(a[1]);
+            return new BigComplex(real, imag);
+        } catch (Exception ex) {
+            LOGGER.warn(SIMPLE_NAME + ": " + ex.getMessage());
+        }
+        return null;
+    }
     
     public BigComplex(BigDecimal real, BigDecimal imag) {
         this.real = real;
